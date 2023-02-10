f2df3b79b729943ca9195db16459208829643af9
Yihyun Choi <yichoi@c3r4s1.42seoul.kr>
2023-02-10 16:53:23 +0900

    Stack imp

--- stack.hpp
+++ stack.hpp
@@ -7,19 +7,61 @@
 
 namespace ft
 {
-	template <typename T, class Container = std::vector<T>>
-	class	stack
+	template <typename T, class _Container = std::vector<T> >
+	class	Stack
 	{
 	private:
-		T	*data;
-		int	size;
-		int	capacity;
-
+		friend bool operator==(const Stack&, const Stack&);
+		friend bool operator<(const Stack&, const Stack&);
 	public:
-		Stack() : size(0), capacity(10) {
-		data = 
-	}
-	};
-}
+		typedef typename _Container::value_type	value_type;
+		typedef typename _Container::size_type	size_type;
+		typedef typename _Container				container_type;
 
+		typedef typename _Container::reference			reference;
+		typedef typename _Container::const_reference	const_reference;
+	protected:
+		_Container	_M_c; // why _M_c?
+	public:
+		Stack() : _M_c() {}
+		explicit	Stack(const _Container& __s) : _M_c(__s) {}
+
+		bool			empty() const { return _M_c.empty(); }
+		size_type		size() const { return _M_c.size(); }
+		reference		top() { return _M.c.back(); }
+		const_reference	top() const { return _M_c.back(); }
+		void			push(const value_type& __x) { _M_c.push_back(__x); }
+		void			pop() { _M_c.pop_back(); }
+	};
+
+	template <typename T, class _Con>
+	bool operator==(const Stack<T, _Con>& __x, const Stack<T, _Con>& __y) {
+		return (__x._M_c == __y._M_c);
+	}
+
+	template <typename T, class _Con>
+	bool operator<(const Stack<T, _Con>& __x, const Stack<T, _Con>& __y) {
+		return (__x._M_c < __y._M_c);
+	}
+
+	template <typename T, class _Con>
+	bool operator!=(const Stack<T, _Con>& __x, const Stack<T, _Con>& __y) {
+		return (__x._M_c != __y._M_c);
+	}
+	
+	template <typename T, class _Con>
+	bool operator>(const Stack<T, _Con>& __x, const Stack<T, _Con>& __y) {
+		return (__x._M_c > __y._M_c);
+	}
+	
+	template <typename T, class _Con>
+	bool operator<=(const Stack<T, _Con>& __x, const Stack<T, _Con>& __y) {
+		return (__x._M_c <= __y._M_c);
+	}
+
+	template <typename T, class _Con>
+	bool operator>=(const Stack<T, _Con>& __x, const Stack<T, _Con>& __y) {
+		return (__x._M_c >= __y._M_c);
+	}
+}
 #endif
