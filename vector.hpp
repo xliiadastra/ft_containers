a76312afec06907c5f737327f3f0b2a1c19e7eaa
Yihyun Choi <yichoi@c3r4s3.42seoul.kr>
2023-02-11 22:51:32 +0900

    vector member

--- vector.hpp
+++ vector.hpp
@@ -19,8 +19,18 @@ public:
 	}
 };
 
+template <typename T, typename Alloc = std::allocator<T> >
 class	vector {
-
+public:
+	typedef T					value_type;
+	typedef value_type&			reference;
+	typedef const value_type&	const_reference;
+	typedef value_type*			pointer;
+	typedef const value_type*	const_pointer;
+	typedef value_type*			iterator;
+	typedef const value_type*	const_iterator;
+	typedef size_t				size_type;
+	typedef ptrdiff_t			difference_type;
 
 };
 
