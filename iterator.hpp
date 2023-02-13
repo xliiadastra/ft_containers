a32a0b54a5f4e424df9beb738cda7faf7c1469bd
Yihyun Choi <yichoi@c3r4s6.42seoul.kr>
2023-02-14 04:48:49 +0900

    iterator

--- iterator.hpp
+++ iterator.hpp
@@ -1,6 +1,13 @@
 #ifndef ITERATOR_HPP
 # define ITERATOR_HPP
 
-
+namespace ft {
+    template<typename _Iterator>
+    class reverse_iterator : public iterator<typename iterator_traits<_Iterator>::iterator_catgory,
+                                             typename iterator_traits<_Iterator>::value_type,
+                                             typename iterator_traits<_Iterator>::difference_type,
+                                             typename iterator_traits<_Iterator>::pointer,
+                                             typename iterator_traits<_Iterator>::reference>
+}
 
 #endif
