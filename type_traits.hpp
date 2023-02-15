ea18e2d4b15fdf873f19b53d287f0d9aa9f4fd49
Yihyun Choi <yichoi@c3r3s3.42seoul.kr>
2023-02-15 21:21:28 +0900

    type_traits

--- type_traits.hpp
+++ type_traits.hpp
@@ -2,7 +2,6 @@
 # define TYPE_TRAITS_HPP
 
 # include <cstddef>
-# include <type_traits>
 
 namespace ft {
 /*
@@ -15,37 +14,73 @@ struct enable_if<true, T> {
     typedef T type;
 };
 
-template <class T>
-struct is_integral : public std::false_type {};
-template <>
-struct is_integral<bool> : public std::true_type {};
-template <>
-struct is_integral<char> : public std::true_type {};
-template <>
-struct is_integral<signed char> : public std::true_type {};
-template <>
-struct is_integral<unsigned char> : public std::true_type {};
-template <>
-struct is_integral<wchar_t> : public std::true_type {};
-template <>
-struct is_integral<char16_t> : public std::true_type {};
-template <>
-struct is_integral<short> : public std::true_type {};
-template <>
-struct is_integral<unsigned short> : public std::true_type {};
-template <>
-struct is_integral<int> : public std::true_type {};
-template <>
-struct is_integral<unsigned int> : public std::true_type {};
-template <>
-struct is_integral<long> : public std::true_type {};
-template <>
-struct is_integral<unsigned long> : public std::true_type {};
-template <>
-struct is_integral<long long> : public std::true_type {};
-template <>
-struct is_integral<unsigned long long> : public std::true_type {};
+/*
+ * remove_const
+ */
+template <typename T>
+struct remove_const { typedef T type; };
 
+template <typename T>
+struct remove_const<const T> { typedef T type; };
+
+/*
+ * remove_volatile
+ */
+template <typename T>
+struct remove_volatile { typedef T type; };
+
+template <typename T>
+struct remove_volatile<volatile T> { typedef T type; };
+
+/*
+ * remove_cv
+ */
+template <typename T>
+struct remove_cv { typedef typename remove_volatile<typename remove_const<T>::type>::type type; };
+
+/*
+ * true or false type
+ */
+struct false_type   { static const bool   value = false; };
+struct true_type    { static const bool    value = true; };
+
+/*
+ * is_integral
+ */
+template <typename T>
+struct ft_is_integral : public false_type {};
+template <>
+struct ft_is_integral<bool> : public true_type {};
+template <>
+struct ft_is_integral<char> : public true_type {};
+template <>
+struct ft_is_integral<signed char> : public true_type {};
+template <>
+struct ft_is_integral<unsigned char> : public true_type {};
+template <>
+struct ft_is_integral<wchar_t> : public true_type {};
+template <>
+struct ft_is_integral<char16_t> : public true_type {};
+template <>
+struct ft_is_integral<short> : public true_type {};
+template <>
+struct ft_is_integral<unsigned short> : public true_type {};
+template <>
+struct ft_is_integral<int> : public true_type {};
+template <>
+struct ft_is_integral<unsigned int> : public true_type {};
+template <>
+struct ft_is_integral<long> : public true_type {};
+template <>
+struct ft_is_integral<unsigned long> : public true_type {};
+template <>
+struct ft_is_integral<unsigned long int> : public true_type {};
+template <>
+struct ft_is_integral<long long> : public true_type {};
+template <>
+struct ft_is_integral<unsigned long long> : public true_type {};
+
+template <typename T>
+struct is_integral : public ft_is_integral<typename remove_cv<T>::type> {};
 }
-
 #endif
